c020018d16f8e0d345a5a848384ed0b8fde371d1
Connor Yates <33985656+tulser@users.noreply.github.com>
2025-01-30 00:16:12 -0500

    Adjust github link in subtitle link array

--- src/links.tsx
+++ src/links.tsx
@@ -6,7 +6,7 @@ import lb_github from "public/static/images/links/github/github-mark-white.svg";
 export const biolinks: BioLinkInfo[] = [
     {
         target_title: "Connor's Github",
-        target_url: "https://github.com/wcyq",
+        target_url: "https://github.com/tulser",
         image_src: lb_github,
     },
 ];
